1e65eb6e38c54ad86c10dc07afe57e07af177bc7
Queren Sales <querenhsales@gmail.com>
2025-01-23 17:00:52 -0300

    criando o report com mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      md: true,
+      json: false,
+      timesTamp: "mmddyyyy_HHMMss",
+    },
   },
 });
