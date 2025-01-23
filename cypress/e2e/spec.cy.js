c51993627cd76848a8297b8ac1c0d5fa831fbada
Queren Sales <querenhsales@gmail.com>
2025-01-23 15:35:52 -0300

    adicionando cenarios de teste e baseUrl

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,24 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://127.0.0.1:5500/#')
+describe('Cenário 1: Carregar a página inicial', () => {
+  before(() => {
+    cy.visit('/');
+  });
+  it.only('verificar o titulo', () => {
+    cy.get('header img')
+    .should('be.visible');
   })
-})
+});
+
+describe.only('Cenário 2: Adicionar uma transação', () => {
+  before(() => {
+    cy.visit('/');
+  });
+  it('Clicar no botão "Nova Transação e adicionar uma entrada"', () => {
+    cy.contains('a', '+ Nova Transação').should('be.visible').click();
+    cy.get('input[name="description"]').type('Dizimo')
+    cy.get('input[name="amount"]').type('500')
+    
+    const data = "2025-01-23"
+    cy.get('input[name="date"]').type(data).click();
+    cy.contains('button', 'Salvar').click();
+  });
+});
